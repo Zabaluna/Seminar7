33a6e9d59cdb81fbd8840a06b2f2c2af35dc172a
Наталья Ярцева <zabaluna@mail.ru>
2022-11-21 15:32:31 +0300

    all in classwork

---
+++ Task46/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 46: Задайте двумерный массив размером m×n, 
+// заполненный случайными целыми числами.
+// m = 3, n = 4.
+// 1 4 8 19
+// 5 -2 33 -2
+// 77 3 8 1
+
+Console.Write("Введите количество строк массива: ");
+int rows=int.Parse(Console.ReadLine());
+
+Console.Write("Введите количество столбцов массива: ");
+int columns=int.Parse(Console.ReadLine());
+
+double[,] array = GetArray(rows, columns, 0, 10);
+PrintArray(array);
+
+double[,] GetArray(int m, int n, int min, int max)
+{
+    double[,] result = new double[m, n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            result[i, j] = new Random().NextDouble()*(max-min);
+        }
+    }
+    return result;
+}
+void PrintArray(double[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i,j]:f5} ");
+        }
+        Console.WriteLine();
+    }
+}
