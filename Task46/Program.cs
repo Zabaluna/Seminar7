72e6626e1002e5da0374b9f4b5a40a2ba704c846
Наталья Ярцева <zabaluna@mail.ru>
2022-11-24 13:48:09 +0300

    int

--- Task46/Program.cs
+++ Task46/Program.cs
@@ -4,35 +4,34 @@
 // 1 4 8 19
 // 5 -2 33 -2
 // 77 3 8 1
-
 Console.Write("Введите количество строк массива: ");
 int rows=int.Parse(Console.ReadLine());
 
 Console.Write("Введите количество столбцов массива: ");
 int columns=int.Parse(Console.ReadLine());
 
-double[,] array = GetArray(rows, columns, 0, 10);
+int[,] array = GetArray(rows, columns, 0, 10);
 PrintArray(array);
 
-double[,] GetArray(int m, int n, int min, int max)
+int[,] GetArray(int m, int n, int min, int max)
 {
-    double[,] result = new double[m, n];
+    int[,] result = new int[m, n];
     for (int i = 0; i < m; i++)
     {
         for (int j = 0; j < n; j++)
         {
-            result[i, j] = new Random().NextDouble()*(max-min);
+            result[i, j] = new Random().Next(min, max);
         }
     }
     return result;
 }
-void PrintArray(double[,] inArray)
+void PrintArray(int[,] inArray)
 {
     for (int i = 0; i < inArray.GetLength(0); i++)
     {
         for (int j = 0; j < inArray.GetLength(1); j++)
         {
-            Console.Write($"{inArray[i,j]:f5} ");
+            Console.Write($"{inArray[i,j]} ");
         }
         Console.WriteLine();
     }
